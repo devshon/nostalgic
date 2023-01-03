3ddda31ba4a3af05a3426aa5aa430ab4a705d23d
devshon <wjdgns200297@gmail.com>
2023-01-03 19:37:42 +0900

    feat: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from 'pages/home'
+import Error from 'pages/error'
+import Blog from 'pages/blog'
 
-export default function App() {
-  return <div className='App'>App</div>
+const App: FC = () => {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/blog' element={<Blog />}></Route>
+          <Route path='*' element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
+
+export default App
